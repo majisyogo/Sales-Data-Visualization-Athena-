960d2497502694b19a7f72ae86c6835d4441c874
Maji <majisyogo0414@gmail.com>
2025-11-22 23:23:03 +0700

    Update sample.sql

--- queries/sample.sql
+++ queries/sample.sql
@@ -1,1 +1,13 @@
-"-- Athena sample query" 
+CREATE DATABASE IF NOT EXISTS sales_db;
+
+CREATE EXTERNAL TABLE IF NOT EXISTS sales_db.sales (
+    date STRING,
+    product STRING,
+    quantity INT,
+    price DOUBLE
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+LOCATION 's3://your-bucket-name/data/';
+
